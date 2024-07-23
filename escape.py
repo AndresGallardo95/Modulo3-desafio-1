e23b6c6ba92121378b7526ea3e920ef57a286b3d
Andrés Gallardo <andresgallardo95@gmail.com>
2024-07-23 18:24:11 -0400

    Se Agregan nuevos parametros a los archivos

--- escape.py
+++ escape.py
@@ -1,12 +1,10 @@
-import math
-
 def calcular_velocidad_escape(g, r):
-    return math.sqrt(2 * g * r)
+    return (2 * g * r) ** 0.5
 
 def main():
     try:
-        r = float(input("Ingrese el radio en kilómetros: ")) * 1000  # Convertimos a metros
-        g = float(input("Ingrese la constante g en m/s^2: "))
+        r = float(input("Ingrese el radio en kilómetros (km): ")) * 1000  # Convertimos a metros
+        g = float(input("Ingrese la constante g en (m/s^2): "))
         velocidad_escape = calcular_velocidad_escape(g, r)
         print(f"La velocidad de escape es {velocidad_escape:.2f} m/s")
     except ValueError:
