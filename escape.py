4398193a08304350df624bbb927cb375c9171c9f
Andrés Gallardo <andresgallardo95@gmail.com>
2024-07-23 18:38:09 -0400

    detalles finales

--- escape.py
+++ escape.py
@@ -6,7 +6,7 @@ def main():
         r = float(input("Ingrese el radio en kilómetros (km): ")) * 1000  # Convertimos a metros
         g = float(input("Ingrese la constante g en (m/s^2): "))
         velocidad_escape = calcular_velocidad_escape(g, r)
-        print(f"La velocidad de escape es {velocidad_escape:.2f} m/s")
+        print(f"La velocidad de escape es {velocidad_escape:.2f} (m/s)")
     except ValueError:
         print("Por favor, ingrese valores numéricos válidos.")
 
